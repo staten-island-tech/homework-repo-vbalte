ef7602285658ca66c9d1dfdaf798199bdfd3d877
Veda <veda.balte@sitechhs.com>
2019-09-25 21:08:36 -0400

    g

---
+++ HW 6: math/app.js
@@ -0,0 +1,34 @@
+const num1= 100;
+const num2 = 50;
+let val; 
+
+//simple math w numbers
+val = num1 + num2;
+val = num1 * num2;
+val = num1 - num2;
+val = num1 / num2;
+val = num1 % num2;
+
+
+// math object 
+
+val= Math.PI;
+val = Math.E; 
+val = Math.round(2.8); 
+val = Math.cell(2.4); //round up 
+val = Math.floor(2.4); //round down
+val = Math.sqrt(64);
+val = Math.abs(-3); 
+val = Math.pow(8,2); //powers
+val = Math.min(2,3,5,6,7,8); //and max too
+val = Math.random(); //gives u random decimal 
+
+
+val = Math.floor(Math.random() * 20 + 1); // random num btw 1-20 but no decimals  
+
+
+
+
+
+console.log(val); 
+
