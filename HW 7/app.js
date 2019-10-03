ab1d4553f4b8fa97b7bda253a220e234b9a6f935
Veda <veda.balte@sitechhs.com>
2019-10-03 17:59:49 -0400

    ok

---
+++ HW 7/app.js
@@ -0,0 +1,17 @@
+//HW 7, spread operator 
+
+const vedz = ['swag', 'awesome', 'wonderful']; 
+
+const swag = ['veda', 'veedsz', 'veds']; 
+
+const wow = featured.concat(swag); 
+
+const wacko = [...vedz, 'veg', ...swag]; 
+
+const wackwacko = [...wacko]; 
+
+
+
+
+
+
